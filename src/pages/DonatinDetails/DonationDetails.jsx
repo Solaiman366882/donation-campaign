11988d7ab6cc20b29b36562d6322df2fe8394df6
Md.Solaiman <md.solaiman366882@gmail.com>
2023-09-26 03:35:22 +0600

    Sweet alert added and all succesfull donation saved to local storage

--- src/pages/DonatinDetails/DonationDetails.jsx
+++ src/pages/DonatinDetails/DonationDetails.jsx
@@ -1,23 +1,67 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
+import swal from 'sweetalert';
 import './DonationDetails.css'
 import { useLoaderData, useParams } from "react-router-dom";
+import { useState } from 'react';
 
 const DonationDetails = () => {
 
     const params =useParams();
-
     const donations = useLoaderData();
-    const selectedDonation = donations?.find(donation => donation.id === parseInt(params.id));
+    const [donation,setDonation] = useState({})
 
-    // useEffect(() => {
-    //     const selectedDonation = donations?.find(donation => donation.id === parseInt(params.id));
+    useEffect(() => {
+        const selectedDonation = donations?.find(donation => donation.id === parseInt(params.id));
 
-    // },[params.id,donation]);
+        setDonation(selectedDonation)
+        
+    },[params.id,donations]);
+    
 
-    const{title,img,text_button_bg,price,description} = selectedDonation || {}
+    const{title,img,text_button_bg,price,description} = donation || {}
 
     const buttonBackgroundColor = {
         backgroundColor:text_button_bg
+    }
+
+    // handle donation button 
+    const handleDonation = () => {
+
+        const totalDonation = [];
+
+        const donationCreated = JSON.parse(localStorage.getItem('totalDonation'));
+        
+        if(!donationCreated){
+            totalDonation.push(donation);
+            localStorage.setItem('totalDonation',JSON.stringify(totalDonation))
+            swal({
+                title: "Good job!",
+                text: "Congrats! you have made your first donation",
+                icon: "success",
+                button: "Ok",
+              });
+        }else{
+            const isExist = donationCreated.find((singleDonation) => singleDonation.id === donation.id)
+            if(isExist){
+                swal({
+                    title: "Opps! Sorry",
+                    text: "You have already made this donation before",
+                    icon: "error",
+                    button: "Ok",
+                  });
+                
+            }
+            else{
+                totalDonation.push(...donationCreated,donation)
+                localStorage.setItem('totalDonation',JSON.stringify(totalDonation))
+                swal({
+                    title: "Good job!",
+                    text: "Thank you so much for your donation",
+                    icon: "success",
+                    button: "Ok",
+                  });
+            }
+        }
     }
 
     return (
@@ -26,7 +70,7 @@ const DonationDetails = () => {
                 <div className="donation-img">
                     <img src={img} alt="" />
                     <div className="price-area">
-                        <button style={buttonBackgroundColor} className="donation-btn">Donate ${price}</button>
+                        <button onClick={handleDonation} style={buttonBackgroundColor} className="donation-btn">Donate ${price}</button>
                     </div>
                 </div>
                 <div className="donation-body">
